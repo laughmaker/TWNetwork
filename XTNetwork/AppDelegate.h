67739dadc781013dfe4e77db9c075ef61065b859
zgs225 <zgs225@gmail.com>
2015-09-02 15:32:14 +0800

    Initial Commit

---
+++ XTNetwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTNetwork
+//
+//  Created by 何振东 on 15/9/2.
+//  Copyright © 2015年 LZA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
